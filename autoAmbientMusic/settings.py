e8560a75ab50182677b1a5576de5bd520a5dfbe3
jeremy <jrmy@tuta.io>
2021-04-01 14:37:39 -0300

    fix allowed_hosts

--- autoAmbientMusic/settings.py
+++ autoAmbientMusic/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@zia()#9y1e@6&clydhd_u&j5e97#f6l36+dah3%vltlkp=4!4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
