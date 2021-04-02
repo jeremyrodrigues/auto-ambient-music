2b21f333428644c0516dbcc795f6eadc4e785d7f
jeremy <jrmy@tuta.io>
2021-04-02 12:38:13 -0300

    fix __str__ time

--- controle/choices.py
+++ controle/choices.py
@@ -1,11 +1,11 @@
 WEEKDAYS = (
-    (6, "Sunday"),
     (0, "Monday"),
     (1, "Tuesday"),
     (2, "Wednesday"),
     (3, "Thursday"),
     (4, "Friday"),
-    (5, "Saturday")
+    (5, "Saturday"),
+    (6, "Sunday"),
 )
 
 VOLUMES = (
